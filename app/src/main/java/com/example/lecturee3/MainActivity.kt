a63d96b8525733b5c77eca03a764756fe29893ac
YushkevichP <7146514@gmail.com>
2024-10-06 21:16:44 +0300

    Refactor - activity and launch first fragment a bit

--- app/src/main/java/com/example/lecturee3/MainActivity.kt
+++ app/src/main/java/com/example/lecturee3/MainActivity.kt
@@ -9,25 +9,38 @@ import androidx.fragment.app.Fragment
 // лекция пять - ресайклеры, адаптер, вью холдер
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//            enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        addFragment(ListFragment())
-
-    }
-
-    fun addFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, fragment)
-            .addToBackStack(null)
-            .commit()
+        if (savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, ListFragment())
+                .commit()
+        }
     }
 }
 
 fun Fragment.pushFragment(counter: Int) {
-    (requireActivity() as MainActivity).addFragment(CounterFragment.getInstance(counter))
+    (requireActivity() as MainActivity)
+        .supportFragmentManager
+        .beginTransaction()
+        .replace(R.id.container, CounterFragment.getInstance(counter))
+        .addToBackStack(null)
+        .commit()
 }
+
+
+
+//fun addFragment(fragment: Fragment) {
+//    supportFragmentManager
+//        .beginTransaction()
+//        .replace(R.id.container, fragment)
+//        .addToBackStack(null)
+//        .commit()
+//}
+
+//fun Fragment.pushFragment(counter: Int) {
+//    (requireActivity() as MainActivity).addFragment(CounterFragment.getInstance(counter))
+//}
