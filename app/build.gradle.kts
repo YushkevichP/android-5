4e70e9770ecbeb854f29a6adc0f5e6e1270ac16d
YushkevichP <7146514@gmail.com>
2024-10-07 20:46:36 +0300

    added nav dependencies in gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -49,4 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("android.arch.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("android.arch.navigation:navigation-ui-ktx:2.8.1")
+
 }
